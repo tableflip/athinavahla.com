a771f0bec26a0d52cf4a480e3eb23f882f8dd889
Bernard Mordan <bernardmordan@gmail.com>
2015-01-22 12:57:12 +0000

    upgrading and migration of site to keystone 2.22

---
+++ keystone.js
@@ -0,0 +1,51 @@
+// Simulate config options from your production environment by
+// customising the .env file in your project's root folder.
+require('dotenv').load();
+
+// Require keystone
+var keystone = require('keystone'),
+	handlebars = require('express3-handlebars');
+
+keystone.init({
+
+	'name': 'Athina Vahla',
+	'brand': 'Athina Vahla',
+
+	'less': 'public',
+	'static': 'public',
+	'favicon': 'public/favicon.ico',
+	'views': 'templates/views',
+	'view engine': 'hbs',
+	
+	'custom engine': handlebars.create({
+		layoutsDir: 'templates/views/layouts',
+		partialsDir: 'templates/views/partials',
+		defaultLayout: 'default',
+		helpers: new require('./templates/views/helpers')(),
+		extname: '.hbs'
+	}).engine,
+	
+	'auto update': true,
+	'session': true,
+	'auth': true,
+	'user model': 'User',
+	'cookie secret': 'ahK@<{#K]HgycM)SxE[_Xivv/DueJkw3hHCeW{9WM0#|u#/Z}E_z3QmMI7"sanbq'
+
+});
+
+keystone.import('models');
+
+keystone.set('locals', {
+	_: require('underscore'),
+	env: keystone.get('env'),
+	utils: keystone.utils,
+	editable: keystone.content.editable
+});
+
+keystone.set('routes', require('./routes'));
+
+keystone.set('nav', {
+	'users': 'users'
+});
+
+keystone.start();
